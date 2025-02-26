fedbd37ff653dedf3b2eadbf644578b9107d3b7c
Diego Morales-Briones <diegomorales-briones@Mac.fritz.box>
2025-02-26 21:47:47 +0100

    update

--- DATA/IQ-tree_concatenated/input/run_iqtree.sh
+++
@@ -1,1 +0,0 @@
-/home/morales/Apps/iqtree-2.0.7-Linux/bin/iqtree2  -m MFP --merge -s concatenated_2419_loci.fa -T 120 -B 1000 -wbtl --seqtype DNA -q concatenated_aln.model --prefix IQtree2_concatenated_2419_loci
